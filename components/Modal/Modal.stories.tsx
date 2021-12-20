a8980b04c6633e6c44d2d0f152b6b025695d48f8
renhang <itrenhang@163.com>
2021-12-20 15:43:55 +0800

    modal 增加事件

--- components/Modal/Modal.stories.tsx
+++ components/Modal/Modal.stories.tsx
@@ -1,26 +1,40 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Modal from './index';
+
 
 export default {
   title: 'Example/Modal',
   component: Modal,
 } as ComponentMeta<typeof Modal>;
-let visible = true
-const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+// let visible = true
+const Template: ComponentStory<typeof Modal> = (args) => {
+  
+  const [visible, setVisible ] = useState(args.visible);
+
+  const onCancelHandle = () => {
+    setVisible(false);
+  }
+
+  useEffect(()=>{
+    setVisible(args.visible)
+  },[args.visible])
+  
+  return (<Modal {...args} onCancel={onCancelHandle} visible={visible} />)
+};
 export const ModalExample = Template.bind({});
 ModalExample.args = {
-  visible: visible,
+  visible: true,
   wrapClassName: 'self-modal',
   // width: 500,
   title: <div>标题d</div>,
   cancelText: '取消',
   okText: '确定',
   children: <div>弹窗内容</div>,
-  onCancel: visible = false,
-  onOK: visible = false,
+  onCancel: () => {},
+  onOK: () => {},
   closable: true,
   // footer: '底部',
   maskClosable: true,
--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,89 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import { fixedScroll, resetScroll } from '../_util/scrollControl';
-import Button from '../Button/index';
-import Icon from '../Icon';
-import {getPrefixCls} from "../_util/responsiveObserve";
+import React, { useState, useEffect } from "react";
+import { fixedScroll, resetScroll } from "../_util/scrollControl";
+import Button from "../Button/index";
+import Icon from "../Icon";
+import { getPrefixCls } from "../_util/responsiveObserve";
 interface ModalProps {
-    visible: Boolean;
-    wrapClassName?: string;
-    width?: number;
-    title?: string | React.ReactNode;
-    cancelText?: string | React.ReactNode;
-    okText?: string | React.ReactNode;
-    children?: any;
-    onCancel?: any;
-    onOK?: any;
-    closable?: boolean;
-    footer?: any;
-    maskClosable?: boolean;
+  visible: Boolean;
+  wrapClassName?: string;
+  width?: number;
+  title?: string | React.ReactNode;
+  cancelText?: string | React.ReactNode;
+  okText?: string | React.ReactNode;
+  children?: any;
+  onCancel?: () => void;
+  onOK?: () => void;
+  closable?: boolean;
+  footer?: any;
+  maskClosable?: boolean;
 }
 
 interface ModalComponentProps extends React.FC<ModalProps> {
-    // confirm: typeof confirm;
+  // confirm: typeof confirm;
 }
-
 
 const Modal: ModalComponentProps = ({
-                                        visible,
-                                        wrapClassName = '',
-                                        width,
-                                        title,
-                                        cancelText = 'Cancel',
-                                        okText = 'OK',
-                                        children,
-                                        onCancel,
-                                        onOK,
-                                        closable = false,
-                                        footer,
-                                        maskClosable = true
-                                    }) => {
-    const prefixCls = getPrefixCls('modal');
-    const [target, setTarget] = useState(null);
-    const [style] = useState(width ? { width: width + 'px' } : {});
-    // 点击关闭
-    const handleCancel = () => onCancel && onCancel();
-    // mask 点击关闭
-    const maskClosableFn = () => maskClosable && handleCancel();
-    // 阻止冒泡
-    const stopPropagationFn = (e) => e.stopPropagation();
-    // 点击确定
-    const handleOK = () => onOK && onOK();
-    useEffect(() => {
-        setTarget(document.getElementById('modal-root') as any);
-    }, []);
-    useEffect(() => {
-        visible ? fixedScroll() : resetScroll();
-    }, [visible]);
+  visible,
+  wrapClassName = "",
+  width,
+  title,
+  cancelText = "Cancel",
+  okText = "OK",
+  children,
+  onCancel,
+  onOK,
+  closable = false,
+  footer,
+  maskClosable = true,
+}) => {
+  const prefixCls = getPrefixCls("modal");
+  const [target, setTarget] = useState(null);
+  const [style] = useState(width ? { width: width + "px" } : {});
+  // 点击关闭
+  const handleCancel = () => onCancel && onCancel();
+  // mask 点击关闭
+  const maskClosableFn = () => maskClosable && handleCancel();
+  // 阻止冒泡
+  const stopPropagationFn = (e) => e.stopPropagation();
+  // 点击确定
+  const handleOK = () => onOK && onOK();
+  useEffect(() => {
+    setTarget(document.getElementById("modal-root") as any);
+  }, []);
+  useEffect(() => {
+    visible ? fixedScroll() : resetScroll();
+  }, [visible]);
 
-    return visible? <div className={`${prefixCls}`}>
-        <div className={`${prefixCls}-mask`} />
-        <div className={`${prefixCls}-warp ${wrapClassName}`} onClick={maskClosableFn}>
-            <div className={`${prefixCls}-content`} style={style} onClick={stopPropagationFn}>
-                <div className={`${prefixCls}-header`}>
-                    {
-                        title &&(<div className={`${prefixCls}-title`}>{title}</div>)
-                    }
-                    {   closable && (
-                        <div className={`${prefixCls}-close-wrapper`} onClick={handleCancel}>
-                            <Icon color="rgba(94, 108, 132, 0.49)" name="a-Crosssign" />
-                        </div>)
-                    }
-                </div>
+  return visible ? (
+    <div className={`${prefixCls}`}>
+      <div className={`${prefixCls}-mask`} />
+      <div
+        className={`${prefixCls}-warp ${wrapClassName}`}
+        onClick={maskClosableFn}
+      >
+        <div
+          className={`${prefixCls}-content`}
+          style={style}
+          onClick={stopPropagationFn}
+        >
+          <div className={`${prefixCls}-header`}>
+            {title && <div className={`${prefixCls}-title`}>{title}</div>}
+            {closable && (
+              <div
+                className={`${prefixCls}-close-wrapper`}
+                onClick={handleCancel}
+              >
+                <Icon color="rgba(94, 108, 132, 0.49)" name="a-Crosssign" />
+              </div>
+            )}
+          </div>
 
-                <div className={`${prefixCls}-body`}>{children}</div>
-                {footer || footer === null ? (
-                    footer
-                ) : (
-                    <div className={`${prefixCls}-footer`}>
-                        <Button  onClick={handleCancel}>{cancelText}</Button>
-                        <Button backgroundColor="#FF8A8A" primary onClick={handleOK}>
-                            {okText}
-                        </Button>
-                    </div>
-                )}
+          <div className={`${prefixCls}-body`}>{children}</div>
+          {footer || footer === null ? (
+            footer
+          ) : (
+            <div className={`${prefixCls}-footer`}>
+              <Button onClick={handleCancel}>{cancelText}</Button>
+              <Button backgroundColor="#FF8A8A" primary onClick={handleOK}>
+                {okText}
+              </Button>
             </div>
+          )}
         </div>
-    </div>:null
-}
+      </div>
+    </div>
+  ) : null;
+};
 
-export default Modal
+export default Modal;
