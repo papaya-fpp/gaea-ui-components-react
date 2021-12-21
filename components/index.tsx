2528a57f7745d3416b3b1f9eab9652982dcad8ca
renhang <itrenhang@163.com>
2021-12-21 15:01:00 +0800

    提交一下

--- components/index.tsx
+++ components/index.tsx
@@ -4,8 +4,6 @@ export { default as Input } from './Input';
 
 export { default as Button } from './Button';
 
-export { default as Header } from './Header';
-
 export { Row, Col } from './Grid';
 
 export { default as Layout } from './Layout';
@@ -18,6 +16,8 @@ export { default as Badge } from './Badge';
 
 export { default as Avatar } from './Avatar';
 
+export { default as Dropdown } from './Dropdown';
+
 export { default as Form } from './Form';
 
 export { default as Modal } from './Modal';
@@ -26,3 +26,4 @@ export { default as Select } from './Select';
 
 export { default as SaveButtonBar } from './SaveButtonBar';
 
+export { default as Icon } from './Icon';
