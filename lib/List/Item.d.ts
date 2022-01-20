349fb32a926b83d2cffb4fb55970cff16ac3e9d6
郭强 <1099594257@qq.com>
2022-01-20 14:19:50 +0800

    run build

--- lib/List/Item.d.ts
+++ lib/List/Item.d.ts
@@ -1,8 +1,7 @@
 import React from 'react';
 export interface ListItemProps extends React.HTMLAttributes<HTMLDivElement> {
     className?: string;
-    prefixCls?: string;
     children?: React.ReactNode;
 }
-declare function ListItem({ className, prefixCls, children }: ListItemProps): JSX.Element;
+declare function ListItem({ className, children, ...others }: ListItemProps): JSX.Element;
 export default ListItem;
